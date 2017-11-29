732ab06304f624be2c91b08d3649deb86889f434
AlistairAlistair <email@alistairguthrie.co>
2017-11-29 20:36:02 +0000

    Pre evening coding

--- db/seeds.rb
+++ db/seeds.rb
@@ -98,6 +98,11 @@ departure4 = Departure.new({
       "departure_city" => "Hanoi",
       "dep_country" => "Vietnam"
       })
+      departure13 = Departure.new({
+        "departure_airport_name" => "Ferenc Liszt International Airport",
+        "departure_city" => "Budapest",
+        "dep_country" => "Hungary"
+        })
 
 departure1.save()
 departure2.save()
@@ -172,6 +177,11 @@ destination1 = Destination.new({
       "destination_city" => "Hanoi",
       "country" => "Vietnam"
       })
+      destination12 = Destination.new({
+      "destination_airport_name" => "Ferenc Liszt International Airport",
+      "destination_city" => "Budapest",
+      "country" => "Hungary"
+      })
 destination1.save()
 destination2.save()
 destination3.save()
@@ -187,14 +197,14 @@ destination12.save()
 
 flight1 = Flight.new({
   "flight_number" => "1234",
-  "ticket_cost" => 2300,
-  "flight_date" => "2017-11-29",
+  "ticket_cost" => 8900,
+  "flight_date" => "2017-12-02",
   "capacity" => 100,
-  "available_seats" => 12,
+  "available_seats" => 93,
   "status" => true,
   "airline_id" => 1,
   "departure_id" => 1,
-  "destination_id" => 6
+  "destination_id" => 8
   })
 
 flight1.save()
@@ -202,41 +212,265 @@ flight1.save()
 flight2 = Flight.new({
   "flight_number" => "2373",
   "ticket_cost" => 18000,
-  "flight_date" => "2017-11-29",
+  "flight_date" => "2017-12-06",
   "capacity" => 100,
   "available_seats" => 12,
   "status" => true,
-  "airline_id" => 2,
-  "departure_id" => 1,
-  "destination_id" => 8
+  "airline_id" => 4,
+  "departure_id" => 8,
+  "destination_id" => 11
   })
 
 flight2.save()
 
 flight3 = Flight.new({
-  "flight_number" => "8302",
+  "flight_number" => "8300",
   "ticket_cost" => 12000,
-  "flight_date" => "2017-11-29",
+  "flight_date" => "2017-12-10",
   "capacity" => 100,
-  "available_seats" => 12,
+  "available_seats" => 53,
   "status" => true,
   "airline_id" => 3,
-  "departure_id" => 2,
-  "destination_id" => 9
+  "departure_id" => 11,
+  "destination_id" => 3
   })
 
 flight3.save()
 
 flight4 = Flight.new({
   "flight_number" => "9385",
-  "ticket_cost" => 8000,
-  "flight_date" => "2017-11-29",
+  "ticket_cost" => 130300,
+  "flight_date" => "2017-12-02",
   "capacity" => 100,
-  "available_seats" => 12,
+  "available_seats" => 03,
   "status" => true,
-  "airline_id" => 4,
+  "airline_id" => 1,
   "departure_id" => 2,
   "destination_id" => 12
   })
 
 flight4.save()
+
+flight5 = Flight.new({
+  "flight_number" => "7482",
+  "ticket_cost" => 23900,
+  "flight_date" => "2017-12-06",
+  "capacity" => 100,
+  "available_seats" => 37,
+  "status" => true,
+  "airline_id" => 2,
+  "departure_id" => 3,
+  "destination_id" => 5
+  })
+
+flight5.save()
+
+flight6 = Flight.new({
+  "flight_number" => "4194",
+  "ticket_cost" => 23100,
+  "flight_date" => "2017-12-10",
+  "capacity" => 100,
+  "available_seats" => 64,
+  "status" => true,
+  "airline_id" => 3,
+  "departure_id" => 3,
+  "destination_id" => 8
+  })
+
+flight6.save()
+
+flight7 = Flight.new({
+  "flight_number" => "1984",
+  "ticket_cost" => 129500,
+  "flight_date" => "2017-12-02",
+  "capacity" => 100,
+  "available_seats" => 86,
+  "status" => true,
+  "airline_id" => 4,
+  "departure_id" => 2,
+  "destination_id" => 1
+  })
+
+flight7.save()
+
+flight8 = Flight.new({
+  "flight_number" => "5382",
+  "ticket_cost" => 89000,
+  "flight_date" => "2017-12-06",
+  "capacity" => 100,
+  "available_seats" => 91,
+  "status" => true,
+  "airline_id" => 5,
+  "departure_id" => 8,
+  "destination_id" => 6
+  })
+
+flight8.save()
+
+flight9 = Flight.new({
+  "flight_number" => "1302",
+  "ticket_cost" => 12300,
+  "flight_date" => "2017-12-10",
+  "capacity" => 100,
+  "available_seats" => 03,
+  "status" => true,
+  "airline_id" => 1,
+  "departure_id" => 4,
+  "destination_id" => 7
+  })
+
+flight9.save()
+
+flight10 = Flight.new({
+  "flight_number" => "1420",
+  "ticket_cost" => 938200,
+  "flight_date" => "2017-12-02",
+  "capacity" => 100,
+  "available_seats" => 35,
+  "status" => true,
+  "airline_id" => 2,
+  "departure_id" => 6,
+  "destination_id" => 10
+  })
+
+flight10.save()
+
+flight11 = Flight.new({
+  "flight_number" => "6482",
+  "ticket_cost" => 94800,
+  "flight_date" => "2017-12-06",
+  "capacity" => 100,
+  "available_seats" => 83,
+  "status" => true,
+  "airline_id" => 3,
+  "departure_id" => 7,
+  "destination_id" => 4
+  })
+
+flight11.save()
+
+flight12 = Flight.new({
+  "flight_number" => "9483",
+  "ticket_cost" => 31900,
+  "flight_date" => "2017-12-10",
+  "capacity" => 100,
+  "available_seats" => 19,
+  "status" => true,
+  "airline_id" => 4,
+  "departure_id" => 2,
+  "destination_id" => 8
+  })
+
+flight12.save()
+
+flight13 = Flight.new({
+  "flight_number" => "6475",
+  "ticket_cost" => 24700,
+  "flight_date" => "2017-12-02",
+  "capacity" => 100,
+  "available_seats" => 36,
+  "status" => true,
+  "airline_id" => 5,
+  "departure_id" => 8,
+  "destination_id" => 10
+  })
+
+flight13.save()
+
+flight14 = Flight.new({
+  "flight_number" => "2190",
+  "ticket_cost" => 84200,
+  "flight_date" => "2017-12-06",
+  "capacity" => 100,
+  "available_seats" => 82,
+  "status" => true,
+  "airline_id" => 1,
+  "departure_id" => 5,
+  "destination_id" => 7
+  })
+
+flight14.save()
+
+flight15 = Flight.new({
+  "flight_number" => "9385",
+  "ticket_cost" => 49400,
+  "flight_date" => "2017-12-10",
+  "capacity" => 100,
+  "available_seats" => 92,
+  "status" => true,
+  "airline_id" => 2,
+  "departure_id" => 9,
+  "destination_id" => 10
+  })
+
+flight15.save()
+
+flight16 = Flight.new({
+  "flight_number" => "9385",
+  "ticket_cost" => 12400,
+  "flight_date" => "2017-12-02",
+  "capacity" => 100,
+  "available_seats" => 13,
+  "status" => true,
+  "airline_id" => 3,
+  "departure_id" => 4,
+  "destination_id" => 3
+  })
+
+flight16.save()
+
+flight17 = Flight.new({
+  "flight_number" => "9385",
+  "ticket_cost" => 8000,
+  "flight_date" => "2017-12-06",
+  "capacity" => 100,
+  "available_seats" => 28,
+  "status" => true,
+  "airline_id" => 4,
+  "departure_id" => 7,
+  "destination_id" => 10
+  })
+
+flight17.save()
+
+flight18 = Flight.new({
+  "flight_number" => "9385",
+  "ticket_cost" => 8000,
+  "flight_date" => "2017-12-10",
+  "capacity" => 100,
+  "available_seats" => 74,
+  "status" => true,
+  "airline_id" => 5,
+  "departure_id" => 6,
+  "destination_id" => 5
+  })
+
+flight18.save()
+
+flight19 = Flight.new({
+  "flight_number" => "9385",
+  "ticket_cost" => 8000,
+  "flight_date" => "2017-12-02",
+  "capacity" => 100,
+  "available_seats" => 91,
+  "status" => true,
+  "airline_id" => 1,
+  "departure_id" => 8,
+  "destination_id" => 6
+  })
+
+flight19.save()
+
+flight20 = Flight.new({
+  "flight_number" => "9385",
+  "ticket_cost" => 8000,
+  "flight_date" => "2017-12-06",
+  "capacity" => 100,
+  "available_seats" => 56,
+  "status" => true,
+  "airline_id" => 2,
+  "departure_id" => 9,
+  "destination_id" => 10
+  })
+
+flight20.save()
